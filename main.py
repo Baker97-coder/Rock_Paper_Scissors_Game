6ab0fe570f02ea135071281273feb252f3b1d6c4
Andrew Baker <baker.d.andrew@gmail.com>
2020-08-17 12:54:34 -0700

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 
 
+# used to redo user input if they input something other than y or n
+
 def redo_start(a):
     if "y" not in a:
         if "n" not in a:
@@ -12,11 +14,13 @@ def redo_start(a):
 
 
 start_game = input("start Rock, Paper, Scissors game? (y) (n): ")
-start_game = redo_start(start_game)
+redo_start(start_game)
 
 while start_game == "y":
     player_move = input("Do you choose paper, rock, or scissors? ")
 
+    # starts computer move and covers tie scenario
+    
     computer_move = random.randint(1, 3)
     if computer_move == 1:
         computer_move = str("rock")
@@ -67,5 +71,8 @@ while start_game == "y":
             if "scissors" not in player_move:
                 print("input is not valid")
                 print()
+
+    # offers user to play another round
+
     start_game = input("would you like to play another round? (y) (n): ")
     redo_start(start_game)
